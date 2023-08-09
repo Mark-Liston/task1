1808be4a57d4a66e45452be97784116546ebe568
Mark Liston <markliston1233901@gmail.com>
2023-08-09 11:30:59 +0800

    Moved IOR passing from args to file
    Server writes IOR to file and client reads it from file. Also
    implemented Naming Service usage but couldn't get it to work so I
    commented it out.

--- server/toaster/client.cpp
+++ server/toaster/client.cpp
@@ -2,6 +2,7 @@
 
 #include "Task1C.h"
 #include <ace/streams.h>
+//#include <orbsvcs/CosNamingC.h>
 
 int main(int argc, char * argv[])
 {
@@ -10,16 +11,24 @@ int main(int argc, char * argv[])
 		// Initialises the ORB.
 		CORBA::ORB_var orb = CORBA::ORB_init(argc, argv, "toasterORB");
 
-		// Checks if IOR was included in arguments.
-		if (argc < 2)
-		{
-			std::cerr << "IOR not given as argument." << std::endl;
-			return 1;
-		}
+		//CORBA::Object_var naming_context_object =
+		//	orb->resolve_initial_references("NameService");
+		//CosNaming::NamingContext_var naming_context =
+		//	CosNaming::NamingContext::_narrow(naming_context_object.in());
 
+		//CosNaming::Name name(1);
+		//name.length(1);
+		//name[0].id = CORBA::string_dup("Toaster");
+
+		//CORBA::Object_var toaster_object = naming_context->resolve(name);
+		//Task1::Toaster_var toaster = Task1::Toaster::_narrow(toaster_object.in());
+
+		// File where object's IOR is written to.
+		const ACE_TCHAR * ior = ACE_TEXT("file://ior_file");
 		// IOR supplied in first argument is used to create the factory object
 		// reference. The naming service should be used for this.
-		CORBA::Object_var toaster_object = orb->string_to_object(argv[1]);
+		//CORBA::Object_var toaster_object = orb->string_to_object(argv[1]);
+		CORBA::Object_var toaster_object = orb->string_to_object(ior);
 
 		// Downcasts object reference to the appropriate type.
 		Task1::Toaster_var toaster =
--- server/toaster/servant.cpp
+++ server/toaster/servant.cpp
@@ -2,8 +2,11 @@
 
 #include "Toaster.h"
 #include <ace/streams.h>
+//#include <orbsvcs/CosNamingC.h>
+
 
 void init_corba(int argc, char * argv[], CORBA::ORB_var & orb, PortableServer::POA_var & poa);
+bool write_ior_to_file(char * path, CORBA::String_var ior);
 void end_corba(CORBA::ORB_var & orb, PortableServer::POA_var & poa);
 
 int main(int argc, char * argv[])
@@ -21,9 +24,33 @@ int main(int argc, char * argv[])
 		// Activates it to obtain the object reference.
 		Task1::Toaster_var toaster = toaster_impl._this();
 
+		
+		//// Gets reference to the Naming Service and narrows it into a usable
+		//// object.
+		//CORBA::Object_var naming_context_object =
+		//	orb->resolve_initial_references("NameService");
+		//CosNaming::NamingContext_var naming_context =
+		//	CosNaming::NamingContext::_narrow(naming_context_object.in());
+		//
+		//// Creates a name object to store the toaster object's name.
+		//CosNaming::Name name(1);
+		//name.length(1);
+		//// Initialises name of object.
+		//name[0].id = CORBA::string_dup("Toaster");
+		//
+		//// Registers the object reference in the naming service. Uses rebind()
+		//// rather than bind() to prevent failure in the event that there is
+		//// already any data there.
+		//naming_context->rebind(name, toaster.in());
+
+
 		// Converts the object reference to an IOR string.
 		CORBA::String_var ior = orb->object_to_string(toaster.in());
-		std::cout << ior.in() << std::endl;
+		char file_name[] = "ior_file";
+		if (!write_ior_to_file(file_name, ior))
+		{
+			std::cerr << "Cannot write to IOR to output file." << std::endl;
+		}
 
 		// Enters listener loop.
 		orb->run();
@@ -56,6 +83,19 @@ void init_corba(int argc, char * argv[], CORBA::ORB_var & orb, PortableServer::P
 	PortableServer::POAManager_var poa_manager = poa->the_POAManager();
 	// Takes POA from holding state to active state.
 	poa_manager->activate();
+}
+
+bool write_ior_to_file(char * path, CORBA::String_var ior)
+{
+	const ACE_TCHAR * ior_file = ACE_TEXT(path);
+	FILE * output_file = ACE_OS::fopen(ior_file, "w");
+	if (output_file == 0)
+	{
+		return false;
+	}
+	ACE_OS::fprintf(output_file, "%s", ior.in());
+	ACE_OS::fclose(output_file);
+	return true;
 }
 
 // Boilerplate for ending CORBA connection.
